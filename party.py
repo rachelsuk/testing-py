2691ceb726911574e4b3c52a840c6f6eeb8b59cb
Rachel <rachelsuk97@gmail.com>
2021-03-05 04:26:44 +0000

    wrote doctests for most_and_least_common_type() in party.py

--- party.py
+++ party.py
@@ -9,6 +9,7 @@ app.secret_key = "SECRETSECRETSECRET"
 
 def is_mel(name, email):
     """Is this user Mel?
+
         >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
         True
         >>> is_mel('Judith Butler', 'judith@awesome.com')
@@ -26,28 +27,56 @@ def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+
+        >>> most_and_least_common_type([\
+            {'type': 'dessert'},\
+            {'type': 'dessert'},\
+            {'type': 'appetizer'},\
+            {'type': 'dessert'},\
+            {'type': 'appetizer'},\
+            {'type': 'drink'},\
+        ])
+        ('dessert', 'drink')
+        >>> most_and_least_common_type([\
+            {'type': 'dessert'},\
+            {'type': 'dessert'},\
+            {'type': 'dessert'},\
+        ])
+        ('dessert', 'dessert')
+        >>> most_and_least_common_type([\
+            {'type': 'dessert'},\
+            {'type': 'dessert'},\
+            {'type': 'appetizer'},\
+            {'type': 'appetizer'},\
+            {'type': 'drink'},\
+        ])
+        ('dessert', 'drink')
+        >>> most_and_least_common_type([])
+        ()
     """
+    if treats:
+        types = {}
 
-    types = {}
+        # Count number of each type
+        for treat in treats:
+            types[treat['type']] = types.get(treat['type'], 0) + 1
 
-    # Count number of each type
-    for treat in treats:
-        types[treat['type']] = types.get(treat['type'], 0) + 1
+        most_count = most_type = None
+        least_count = least_type = None
 
-    most_count = most_type = None
-    least_count = least_type = None
+        # Find most, least common
+        for treat_type, count in types.items():
+            if most_count is None or count > most_count:
+                most_count = count
+                most_type = treat_type
 
-    # Find most, least common
-    for treat_type, count in types.items():
-        if most_count is None or count > most_count:
-            most_count = count
-            most_type = treat_type
+            if least_count is None or count < least_count:
+                least_count = count
+                least_type = treat_type
 
-        if least_count is None or count < least_count:
-            least_count = count
-            least_type = treat_type
-
-    return (most_type, least_type)
+        return (most_type, least_type)
+    else:
+        return ()
 
 
 def get_treats():
